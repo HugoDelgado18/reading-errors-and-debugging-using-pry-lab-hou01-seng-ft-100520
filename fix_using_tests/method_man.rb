c1ed7395a2c58411af142f10ca6fb0bb7f2e15c3
Hugo Delgado <hugo.delgado1@outlook.com>
2020-07-30 17:32:19 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -9,6 +9,7 @@ def start_game(player1, player2)
 end
 
 def play_game(player1, player2)
+  binding.pry
   player1 = "Jay-Z"
   player2 = "Beyonce"
   statement = "#{player1} is better than #{player2}"
