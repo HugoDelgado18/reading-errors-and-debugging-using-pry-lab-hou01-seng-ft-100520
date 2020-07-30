160051a094a1884d47e154dd13d3d1d5d2f80663
Hugo Delgado <hugo.delgado1@outlook.com>
2020-07-30 06:45:21 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,15 +1,16 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
+  binding.pry
   player1 = "Rocky"
   player2 = "Bullwinkle"
   greetings = "Hello #{player1} & #{playar2}"
-  puts greetings
+  print greetings
 end
 
 def play_game(player1, player2)
   player1 = "Jay-Z"
   player2 = "Beyonce"
   greetings = "#{player1} is better than #{player2}"
-  puts greetings
+  print greetings
 end
