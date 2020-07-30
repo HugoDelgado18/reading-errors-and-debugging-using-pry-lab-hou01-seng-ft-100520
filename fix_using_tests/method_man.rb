ab970ebd4c65c4c6bcefa7ff51d6a9ea41f02f82
Hugo Delgado <hugo.delgado1@outlook.com>
2020-07-30 06:23:28 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -5,11 +5,11 @@ def start_game(player1, player2)
   player2 = ""
   "Hello #{player1} & #{playar2}"
   binding.pry
- puts 'Hello'
+
 end
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
   binding.pry
-  puts "hello"
+
 end
